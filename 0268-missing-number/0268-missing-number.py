351caa1080220931c82f070b45b22619034c9536
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-09-17 16:31:47 +0530

    Time: 1409 ms (8.33%), Space: 12.8 MB (69.66%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+            
+            
+
+            
+
+
+        
