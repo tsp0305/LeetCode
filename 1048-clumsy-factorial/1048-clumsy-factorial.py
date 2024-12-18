584a1b8b29928c8413ddae51d5b29cc2ddb9c12f
tsp0305 <162017015+tsp0305@users.noreply.github.com>
2024-12-18 10:29:35 +0530

    Time: 15 ms (60.69%), Space: 18.1 MB (26.32%) - LeetHub

--- 1048-clumsy-factorial/1048-clumsy-factorial.py
+++ 1048-clumsy-factorial/1048-clumsy-factorial.py
@@ -1,16 +1,9 @@
 class Solution:
     def clumsy(self, n: int) -> int:
-        # Stack that we will use to perform the calculations
-        stack = [n]
         
-        # Which operation to perform
-        # 1 -> Multiplication
-        # 2 -> Division
-        # 3 -> Addition
-        # 4 -> Subtraction
+        stack = [n]
         operation = 1
         
-        # Go through each value from n - 1 to 1
         for i in range(n-1, 0, -1):
             operation %= 4
             
@@ -24,6 +17,6 @@ class Solution:
                 
             operation += 1
         
-        # Finally, just return the sum of all values in the stack
+       
         return sum(stack)
         
